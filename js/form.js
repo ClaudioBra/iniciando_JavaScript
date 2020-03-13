641870a26e1828720b5f6a793026612aef7ec8b0
Claudio <claudio.santos57@fatec.sp.gov.br>
2020-03-13 12:39:28 -0300

    Atualizando o JS

--- js/form.js
+++ js/form.js
@@ -38,7 +38,7 @@ function montaTr(paciente) {
     pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
     pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
     pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
-    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gorduraTd"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
     pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
     return pacienteTr;
